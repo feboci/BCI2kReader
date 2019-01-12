1943b2a5f0da253fc261debfb3d97cba9b9905f8
Markus Adamek <markus.adamek@gmail.com>
2019-01-11 22:35:02 -0500

    changed testcase name to be valid testcase ..

--- pytest.py
+++ pytest.py
@@ -1,10 +1,17 @@
 import unittest
 from BCI2kReader import BCI2kReader as b2k
+import os
 
 class TestStartup(unittest.TestCase):
-	def TestSimpleFileLoading(self):
-		with b2k.BCI2kReader('/TestData/eeg1_1.dat') as file:
-			self.assertIsNotNone(file)
+	def test_SimpleFileLoading(self):
+		dirname = os.path.dirname(__file__)
+		filename = os.path.join(dirname, 'TestData\eeg1_1.dat')
+		print(filename)
+		file=b2k.BCI2kReader(filename)
+		self.assertIsNotNone(file)
+		file.close()
+			
+			
 
 if __name__ == "__main__":
 	unittest.main()
